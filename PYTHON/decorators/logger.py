1c0cb3d13a41d87fce35b2283a6c65df1ac01625
Kaizen-Mage <simulationworld4@gmail.com>
2026-08-31 14:53:45 +0300

    Logger Project

---
+++ PYTHON/decorators/logger.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime,date
+def logger(func):
+    def wrapper(*args,**kwargs):
+        start_time=time.time()
+        result=func(*args,**kwargs)
+        end_time=time.time()
+        today=date.today()
+        duration=round((end_time-start_time)*1000,2)
+        timestamp=datetime.now().isoformat()
+        with open(f"{today}.txt","a") as f:
+            f.write(f"{timestamp} : function:{func.__name__}  duration:{duration}.ms\n")
+    return wrapper
+@logger
+def hello():
+    print("hello")
+hello()
